b99fe9dd584112670a0d6dd20f5d0d2402bbadea
Vitor Cunha <vitorsilvarosa1902@gmail.com>
2025-08-13 20:56:42 -0300

    Metodos criado, RestHighLevelClient e EntityMapper no pacote ElasticSearchConfig

--- src/main/java/com/example/product_catalog/config/ElasticSearchConfig.java
+++ src/main/java/com/example/product_catalog/config/ElasticSearchConfig.java
@@ -1,11 +1,34 @@
 package com.example.product_catalog.config;
 
+import org.elasticsearch.client.RestClient;
+import org.springframework.boot.autoconfigure.web.client.RestClientSsl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
 @EnableElasticsearchRepositories (basePackages = "one.digitalinnovation.experts.productcatalog.repository")
-public class ElasticSearchConfig {
+public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
+    @Override
+    public RestHighLevelClient elasticsearchClient() {
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo("localhost:9200", "localhost:9300")
+                .build();
+        return RestClients.create(clientConfiguration).rest();
+    }
 
+    @Bean
+    @Override
+    public EntityMapper entityMapper() {
+
+        ElasticsearchEntityMapper entityMapper = new ElasticsearchEntityMapper(elasticsearchMappingContext(),
+                new DefaultConversionService());
+                entityMapper.setConversions(elasticsearchCustomConversions());
+
+                return entityMapper;
+    }
 }
