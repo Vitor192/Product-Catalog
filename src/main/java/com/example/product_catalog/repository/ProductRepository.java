83ceb6173800f77e491da0ed1deb485369c8383d
Vitor Cunha <vitorsilvarosa1902@gmail.com>
2025-08-14 18:21:57 -0300

    Pacote model criado, classe Product finalizada e classe ProducRepository sendo desenvolvido

---
+++ src/main/java/com/example/product_catalog/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.example.product_catalog.repository;
+
+import com.example.product_catalog.model.Product;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+}
