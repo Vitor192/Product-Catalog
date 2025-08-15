7bc7201753993bba773c70e156f0d35af6c8a92d
Vitor Cunha <vitorsilvarosa1902@gmail.com>
2025-08-14 21:09:03 -0300

    Microsserviço criado Shopping-cart

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,10 @@ plugins {
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
+<<<<<<< HEAD
+=======
+description = "Shopping-cart"
+>>>>>>> 23f7afd (Microsserviço criado, shopping-cart, pacotes criados como controller e classe CartController, com métodos addItem, buscar carrinho por ID e clear; pacotes model com classes Cart e Item; repository com CartRepository; e pacote config com RedisConfig)
 
 java {
 	toolchain {
@@ -20,6 +24,10 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
+<<<<<<< HEAD
+=======
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+>>>>>>> 23f7afd (Microsserviço criado, shopping-cart, pacotes criados como controller e classe CartController, com métodos addItem, buscar carrinho por ID e clear; pacotes model com classes Cart e Item; repository com CartRepository; e pacote config com RedisConfig)
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
