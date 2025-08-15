6eadce8d203c0f9101e2bcf2af6a55f18e85ceee
Vitor Cunha <vitorsilvarosa1902@gmail.com>
2025-08-14 21:12:22 -0300

    Adicionando microserviço Shopping-cart

--- settings.gradle.kts
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-rootProject.name = "product-catalog"
-=======
-rootProject.name = "Shopping-cart"
->>>>>>> 23f7afd (Microsserviço criado, shopping-cart, pacotes criados como controller e classe CartController, com métodos addItem, buscar carrinho por ID e clear; pacotes model com classes Cart e Item; repository com CartRepository; e pacote config com RedisConfig)
